c8a94a534c128dc80177587eb85dbbfb71ace803
Diego Braga <diego.braga@gmail.com>
2022-01-10 14:45:07 -0300

    insert search in photos

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 <template>
   <div class="corpo">
     <h1 class="centralizado">{{ titulo }}</h1>
-
+    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtro por título" >
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltros">
         <meu-painel :titulo="foto.titulo">
             <img  class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
         </meu-painel>
@@ -27,9 +27,19 @@ export default {
     return {
       titulo: "alura pic",
       subtitulo: "Sub Titulo",
-      fotos: [
-      ],
+      fotos: [],
+      filtro: ''
     };
+  },
+  computed: {
+    fotosComFiltros(){
+      if(this.filtro){
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo))
+      }else{
+        return this.fotos;
+      }
+    }
   },
   created(){
     this.fotos = this.$http.get('http://localhost:3000/v1/fotos')
@@ -61,5 +71,8 @@ export default {
  .imagem-responsiva{
    width: 100%;
  }
-
+  .filtro{
+    display: block;
+    width: 100%;
+  }
 </style>
