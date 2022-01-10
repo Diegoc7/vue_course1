619dff11b328b4933eea7f2b033de58376d42e17
Diego Braga <diego.braga@gmail.com>
2022-01-10 16:51:46 -0300

    enable routes in application

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,13 @@
 
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
-    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtro por título" >
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltros">
-        <meu-painel :titulo="foto.titulo">
-            <imagem-responsiva :src="foto.url" :titulo="foto.titulo"></imagem-responsiva>
-        </meu-painel>
-      </li>
-    </ul>
-
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Painel from './components/shared/painel/Painel.vue';
-import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
 
 export default {
-
-    components: {
-      'meu-painel' : Painel,
-      'imagem-responsiva' : ImagemResponsiva
-    },
-
-  data() {
-    return {
-      titulo: "alura pic",
-      subtitulo: "Sub Titulo",
-      fotos: [],
-      filtro: ''
-    };
-  },
-  computed: {
-    fotosComFiltros(){
-      if(this.filtro){
-        let exp = new RegExp(this.filtro.trim(), 'i');
-        return this.fotos.filter(foto => exp.test(foto.titulo))
-      }else{
-        return this.fotos;
-      }
-    }
-  },
-  created(){
-    this.fotos = this.$http.get('http://localhost:3000/v1/fotos')
-    .then(res => res.json())
-    .then(fotos => this.fotos = fotos, err => console.log(err));
-    
-  }
 };
 </script >
 
@@ -59,20 +18,4 @@ export default {
    margin: 0 auto;
  }
 
- .centralizado{
-   text-align: center;
- }
-
- .lista-fotos{
-   list-style: none;
- }
-
- .lista-fotos .lista-fotos-item{
-   display: inline-block;
- }
- 
-  .filtro{
-    display: block;
-    width: 100%;
-  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+
+
+import { routes } from './routes';
 
 Vue.use(VueResource);
 
+const router = new VueRouter({ 
+  routes ,
+  mode: 'history'
+});
+//é a mesma coisa de:
+// const router = new VueRouter({
+  //   routes: routes
+  // });
+  
+  Vue.use(VueRouter);
+
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
